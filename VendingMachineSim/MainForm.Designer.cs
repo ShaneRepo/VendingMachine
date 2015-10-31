c11a07cadf641d2ac85625572edca3c1a5f34166
Shane <shane8641@live.com>
2015-10-31 01:03:32 -0400

    added listbox and display button on form

--- VendingMachineSim/MainForm.Designer.cs
+++ VendingMachineSim/MainForm.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.listBoxVending = new System.Windows.Forms.ListBox();
+            this.buttonLoadMachine = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(217, 13);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(288, 30);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Vending Machine Simulator";
+            // 
+            // listBoxVending
+            // 
+            this.listBoxVending.FormattingEnabled = true;
+            this.listBoxVending.Location = new System.Drawing.Point(341, 144);
+            this.listBoxVending.Name = "listBoxVending";
+            this.listBoxVending.Size = new System.Drawing.Size(120, 95);
+            this.listBoxVending.TabIndex = 1;
+            // 
+            // buttonLoadMachine
+            // 
+            this.buttonLoadMachine.Location = new System.Drawing.Point(53, 144);
+            this.buttonLoadMachine.Name = "buttonLoadMachine";
+            this.buttonLoadMachine.Size = new System.Drawing.Size(127, 23);
+            this.buttonLoadMachine.TabIndex = 2;
+            this.buttonLoadMachine.Text = "Display Items For Sale";
+            this.buttonLoadMachine.UseVisualStyleBackColor = true;
+            this.buttonLoadMachine.Click += new System.EventHandler(this.buttonLoadMachine_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(746, 628);
-            this.Name = "Form1";
+            this.Controls.Add(this.buttonLoadMachine);
+            this.Controls.Add(this.listBoxVending);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "MainForm";
             this.Text = "Vending Machine Application";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.ListBox listBoxVending;
+        private System.Windows.Forms.Button buttonLoadMachine;
     }
 }
 
--- VendingMachineSim/MainForm.cs
+++ VendingMachineSim/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace VendingMachineSim
 {
@@ -15,6 +16,11 @@ namespace VendingMachineSim
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonLoadMachine_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
