42986a5fa46ea22ccf7c8e265c7a5ede349e5810
Shane <shane8641@live.com>
2015-10-31 12:41:16 -0400

    added 3 listboxes for name, price, quantity

--- VendingMachineSim/MainForm.Designer.cs
+++ VendingMachineSim/MainForm.Designer.cs
@@ -29,8 +29,10 @@
         private void InitializeComponent()
         {
             this.labelTitle = new System.Windows.Forms.Label();
-            this.listBoxVending = new System.Windows.Forms.ListBox();
+            this.listBoxName = new System.Windows.Forms.ListBox();
             this.buttonLoadMachine = new System.Windows.Forms.Button();
+            this.listBoxPrice = new System.Windows.Forms.ListBox();
+            this.listBoxQuantity = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // labelTitle
@@ -43,13 +45,13 @@
             this.labelTitle.TabIndex = 0;
             this.labelTitle.Text = "Vending Machine Simulator";
             // 
-            // listBoxVending
+            // listBoxName
             // 
-            this.listBoxVending.FormattingEnabled = true;
-            this.listBoxVending.Location = new System.Drawing.Point(341, 144);
-            this.listBoxVending.Name = "listBoxVending";
-            this.listBoxVending.Size = new System.Drawing.Size(120, 95);
-            this.listBoxVending.TabIndex = 1;
+            this.listBoxName.FormattingEnabled = true;
+            this.listBoxName.Location = new System.Drawing.Point(210, 144);
+            this.listBoxName.Name = "listBoxName";
+            this.listBoxName.Size = new System.Drawing.Size(161, 95);
+            this.listBoxName.TabIndex = 1;
             // 
             // buttonLoadMachine
             // 
@@ -61,16 +63,35 @@
             this.buttonLoadMachine.UseVisualStyleBackColor = true;
             this.buttonLoadMachine.Click += new System.EventHandler(this.buttonLoadMachine_Click);
             // 
+            // listBoxPrice
+            // 
+            this.listBoxPrice.FormattingEnabled = true;
+            this.listBoxPrice.Location = new System.Drawing.Point(384, 144);
+            this.listBoxPrice.Name = "listBoxPrice";
+            this.listBoxPrice.Size = new System.Drawing.Size(146, 95);
+            this.listBoxPrice.TabIndex = 3;
+            // 
+            // listBoxQuantity
+            // 
+            this.listBoxQuantity.FormattingEnabled = true;
+            this.listBoxQuantity.Location = new System.Drawing.Point(543, 144);
+            this.listBoxQuantity.Name = "listBoxQuantity";
+            this.listBoxQuantity.Size = new System.Drawing.Size(146, 95);
+            this.listBoxQuantity.TabIndex = 4;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(746, 628);
+            this.Controls.Add(this.listBoxQuantity);
+            this.Controls.Add(this.listBoxPrice);
             this.Controls.Add(this.buttonLoadMachine);
-            this.Controls.Add(this.listBoxVending);
+            this.Controls.Add(this.listBoxName);
             this.Controls.Add(this.labelTitle);
             this.Name = "MainForm";
             this.Text = "Vending Machine Application";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,8 +100,10 @@
         #endregion
 
         private System.Windows.Forms.Label labelTitle;
-        private System.Windows.Forms.ListBox listBoxVending;
+        private System.Windows.Forms.ListBox listBoxName;
         private System.Windows.Forms.Button buttonLoadMachine;
+        private System.Windows.Forms.ListBox listBoxPrice;
+        private System.Windows.Forms.ListBox listBoxQuantity;
     }
 }
 
--- VendingMachineSim/MainForm.cs
+++ VendingMachineSim/MainForm.cs
@@ -13,14 +13,72 @@ namespace VendingMachineSim
 {
     public partial class MainForm : Form
     {
+        char[] delim = { ',' };
+        string name = "Name";
+        string price = "Price";
+        string quantity = "Quantity";
+        string border = "------------------";
+        string line;
+        string[] tokens;
+        StreamReader file;
         public MainForm()
         {
             InitializeComponent();
         }
-
+        
+        
         private void buttonLoadMachine_Click(object sender, EventArgs e)
-        {
+        {        
+            try
+            {
+                file = File.OpenText("JunkFood.csv");
+                while (!file.EndOfStream)
+                {
+                    line = file.ReadLine();
+                    tokens = line.Split(delim);
+                    listBoxName.Items.Add(tokens[0]);
+                    listBoxPrice.Items.Add(tokens[1]);
+                    listBoxQuantity.Items.Add(tokens[2]);
+                    //for (int i = 0; i < tokens.Length; i++)
+                    //{
+                    //    listBoxName.Items.Add(tokens[i]);
+                    //    listBoxPrice.Items.Add(tokens[i + 1]);
+                    //    listBoxQuantity.Items.Add(tokens[i + 2]);
+                    //}
+                    //for (int j = 1; j < tokens.Length; j++)
+                    //{
+                    //    listBoxPrice.Items.Add(tokens[j]);
+                    //}
+                    //for (int k = 2; k < tokens.Length; k++)
+                    //{
+                    //    listBoxQuantity.Items.Add(tokens[k]);
+                    //}
+                            
+                        
+                        
+                    
+                    //listBoxVending.Items.Add(line);
+                    //listBoxName.Items.Add(border);
+                }
+            }                
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                file.Close();
+            }
+        }
 
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            listBoxName.Items.Add(name);
+            listBoxName.Items.Add(border);
+            listBoxPrice.Items.Add(price);
+            listBoxPrice.Items.Add(border);
+            listBoxQuantity.Items.Add(quantity);
+            listBoxQuantity.Items.Add(border);
         }
     }
 }
